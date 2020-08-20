79908336a7f1f13f9b3702dc1579c05b6471d56d
Valere21 <62430274+Valere21@users.noreply.github.com>
2020-08-20 16:10:38 +0200

    Add files via upload

---
+++ FTP_POUR_SPECINOV/commande_ftp.cmd
@@ -0,0 +1,10 @@
+rem open ftp.specinov.fr
+open ftp.specinov.fr
+irigo_pc_indus
+Indus_49FTP!
+binary
+cd /Time_Paris
+lcd C:\FTP
+mput *.txt 
+quit
+
---
+++ FTP_POUR_SPECINOV/connexion_ftp.cmd
@@ -0,0 +1,8 @@
+C:
+cd/
+cd FTP\
+
+ftp -i -s:C:\FTP\FTP_POUR_SPECINOV\commande_ftp.cmd
+
+cd c:\FTP\
+move c:\FTP\*.txt c:\FTP\_Archives
